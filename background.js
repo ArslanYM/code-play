a85a9925fc8aa92e6a4d55c55695a12a74deb332
ArslanYM <arsalanyaqmalik@gmail.com>
2024-02-06 14:37:41 +0530

    fixed ui

--- background.js
+++
@@ -1,8 +0,0 @@
-const playlistID = "" //get from env
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({playlistId: `${playlistID}`}, function() {
-      console.log('Playlist ID set');
-    });
-  });
-
